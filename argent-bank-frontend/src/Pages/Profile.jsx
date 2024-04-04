e5beb0f9ad8a8d224b6f22e63d422a377280aa0d
sanadevv <sana.bendaamar@gmail.com>
2024-04-04 09:55:16 +0200

    adding the edit form component

--- argent-bank-frontend/src/Pages/Profile.jsx
+++ argent-bank-frontend/src/Pages/Profile.jsx
@@ -1,79 +1,11 @@
-import React, { useState } from "react";
 import Account from "../components/Account";
-import { useDispatch, useSelector } from "react-redux";
-import { editUserName } from "../actions/authActions";
-const Profile = () => {
-  const dispatch = useDispatch();
-  const userName = useSelector((state) => state.auth.userName);
-  const [editing, setEditing] = useState(false);
-  const [NewUserName, setNewUserName] = useState(userName);
-  const handleEditClic = () => {
-    setEditing(true);
-  };
-  const handleCancel = () => {
-    setEditing(false);
-    setNewUserName(userName);
-  };
-  const handleSave = (e) => {
-    e.preventDefault();
-    dispatch(editUserName(NewUserName));
-    setEditing(false);
-  };
+import EditForm from "../components/EditForm";
 
+const Profile = () => {
   return (
     <div>
       <main className="main bg-dark">
-        <div className="header">
-          {!editing && (
-            <div>
-              <h1>
-                Welcome back
-                <br />
-                {NewUserName}!
-              </h1>
-              <button className="edit-button" onClick={handleEditClic}>
-                Edit Name
-              </button>
-            </div>
-          )}
-          {editing && (
-            <section className="edit-content">
-              <h2>Edit user info</h2>
-              <form onSubmit={handleSave}>
-                <div className="input-wrapper">
-                  <label htmlFor="username">Username:</label>
-                  <input
-                    type="text"
-                    id="username"
-                    value={NewUserName}
-                    onChange={(e) => setNewUserName(e.target.value)}
-                    autoFocus={true}
-                  />
-                </div>
-                <div className="input-wrapper">
-                  <label htmlFor="first-name">First name:</label>
-                  <input type="text" id="first-name" value={""} disabled />
-                </div>
-                <div className="input-wrapper">
-                  <label htmlFor="last-name">Last name:</label>
-                  <input type="text" id="last-name" value={""} disabled />
-                </div>
-                <div className="button-wrapper">
-                  <button type="submit" className="sign-in-button">
-                    Save
-                  </button>
-                  <button
-                    type="button"
-                    className="sign-in-button"
-                    onClick={handleCancel}
-                  >
-                    Cancel
-                  </button>
-                </div>
-              </form>
-            </section>
-          )}
-        </div>
+        <EditForm />
         <h2 className="sr-only">Accounts</h2>
         <Account
           title="Argent Bank Checking (x8349)"
---
+++ argent-bank-frontend/src/components/EditForm.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { editUserName } from "../actions/authActions";
+
+const EditForm = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector((state) => state.auth.userName);
+  const [editing, setEditing] = useState(false);
+  const [NewUserName, setNewUserName] = useState(userName);
+  const handleEditClic = () => {
+    setEditing(true);
+  };
+  const handleCancel = () => {
+    setEditing(false);
+    setNewUserName(userName);
+  };
+  const handleSave = (e) => {
+    e.preventDefault();
+    dispatch(editUserName(NewUserName));
+    setEditing(false);
+  };
+  return (
+    <div>
+      <div className="header">
+        {!editing && (
+          <div>
+            <h1>
+              Welcome back
+              <br />
+              {NewUserName}!
+            </h1>
+            <button className="edit-button" onClick={handleEditClic}>
+              Edit Name
+            </button>
+          </div>
+        )}
+        {editing && (
+          <section className="edit-content">
+            <h2>Edit user info</h2>
+            <form onSubmit={handleSave}>
+              <div className="input-wrapper">
+                <label htmlFor="username">Username:</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={NewUserName}
+                  onChange={(e) => setNewUserName(e.target.value)}
+                  autoFocus={true}
+                />
+              </div>
+              <div className="input-wrapper">
+                <label htmlFor="first-name">First name:</label>
+                <input type="text" id="first-name" value={""} disabled />
+              </div>
+              <div className="input-wrapper">
+                <label htmlFor="last-name">Last name:</label>
+                <input type="text" id="last-name" value={""} disabled />
+              </div>
+              <div className="button-wrapper">
+                <button type="submit" className="sign-in-button">
+                  Save
+                </button>
+                <button
+                  type="button"
+                  className="sign-in-button"
+                  onClick={handleCancel}
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </section>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default EditForm;
