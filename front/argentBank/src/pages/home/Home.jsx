34d8146c0f02dbc1ff058dacdc94d7dd362ad613
RKL715 <remykennyl715@gmail.com>
2024-05-10 14:38:20 +0200

    Added main section content /home

--- front/argentBank/src/pages/home/Home.jsx
+++ front/argentBank/src/pages/home/Home.jsx
@@ -4,7 +4,55 @@ function Home () {
     return (
         <div>
             <Header />
-            <h1>Welcome to Argent Bank</h1>
+            <main>
+                <div className="hero">
+                    <section className="hero-content">
+                        <h2 className="sr-only">Promoted Content</h2>
+                        <p className="subtitle">No fees.</p>
+                        <p className="subtitle">No minimum deposit.</p>
+                        <p className="subtitle">High interest rates.</p>
+                        <p className="text">Open a savings account with Argent Bank today!</p>
+                    </section>
+                </div>
+                <section className="features">
+                    <h2 className="sr-only">Features</h2>
+                    <div className="feature-item">
+                        <img
+                            src={"../public/img/icon-chat.webp"}
+                            alt="Chat Icon"
+                            className="feature-icon"/>
+                        <h3 className="feature-item-title">You are our #1 priority</h3>
+                        <p>
+                            Need to talk to a representative? You can get in touch through our
+                            24/7 chat or through a phone call in less than 5 minutes.
+                        </p>
+                    </div>
+                    <div className="feature-item">
+                        <img
+                            src={"../public/img/icon-money.webp"}
+                            alt="Money Icon"
+                            className="feature-icon"
+                        />
+                        <h3 className="feature-item-title">More savings means higher rates</h3>
+                        <p>
+                            The more you save with us, the higher your interest rate will be!
+                        </p>
+                    </div>
+                    <div className="feature-item">
+                        <img
+                            src={"../public/img/icon-security.webp"}
+                            alt="Security Icon"
+                            className="feature-icon"
+                        />
+                        <h3 className="feature-item-title">Security you can trust</h3>
+                        <p>
+                            We use top of the line encryption to make sure your data and money
+                            is always safe.
+                        </p>
+                    </div>
+                </section>
+            </main>
+
         </div>
     )
 }
