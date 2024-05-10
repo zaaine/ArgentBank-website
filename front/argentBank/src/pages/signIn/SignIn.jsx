8d9afd2d122db1de7be8724f9020ee2f320e1fe8
RKL715 <remykennyl715@gmail.com>
2024-05-10 15:31:30 +0200

    Added signIn page + style

--- front/argentBank/src/pages/signIn/SignIn.jsx
+++ front/argentBank/src/pages/signIn/SignIn.jsx
@@ -1,8 +1,30 @@
 function SignIn () {
     return (
-        <div>
-            <h2>Sign In form here</h2>
-        </div>
+        <main className="main bg-dark">
+            <section className="sign-in-content">
+                <i className="fa fa-user-circle sign-in-icon"></i>
+                <h1>Sign In</h1>
+                <form>
+                    <div className="input-wrapper">
+                        <label htmlFor="username">Username</label
+                        ><input type="text" id="username"/>
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="password">Password</label
+                        ><input type="password" id="password"/>
+                    </div>
+                    <div className="input-remember">
+                        <input type="checkbox" id="remember-me"/><label htmlFor="remember-me"
+                    >Remember me</label
+                    >
+                    </div>
+                    {/*PLACEHOLDER DUE TO STATIC SITE */}
+                    <a href="./User" className="sign-in-button">Sign In</a>
+                    {/*SHOULD BE THE BUTTON BELOW*/}
+                    {/*<button class="signIn-button">Sign In</button> */}
+                </form>
+            </section>
+        </main>
     )
 }
 
