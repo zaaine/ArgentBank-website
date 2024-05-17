80acbce43b7c631a5a46f758bdfac739eb7d6fff
ChabinDev <chabin_972@hotmail.fr>
2024-05-17 16:23:04 +0200

    Création des Fichiers index, settings .scss

---
+++ argentBank/src/components/Navigation.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import { FaUserCircle } from "react-icons/fa";
+
+const Navigation = () => {
+  return (
+    <nav className="main-nav">
+      <Logo />
+      <NavLink
+        to="/Sign_In"
+        className={(nav) => (nav.isActive ? "main-nav-item" : "main-nav-item")}
+      >
+        <i className="fa fa-user-circle">
+          <FaUserCircle />
+        </i>
+        Sign In
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Navigation;
