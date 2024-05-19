5cee45e5f72294145c1fe323e619c0bf19890584
ChabinDev <chabin_972@hotmail.fr>
2024-05-19 17:18:01 +0200

    Mise à jour Store.js

--- argentBank/src/app/store.js
+++ argentBank/src/app/store.js
@@ -1,29 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers/rootReducer";
-import { persistReducer, persistStore } from "redux-persist";
 
-import storage from "redux-persist/lib/storage";
+// RTK Slices
+import { api } from "./api";
+import authReducer from "./reducers/auth.reducer";
+import userReducer from "./reducers/user.reducer";
 
-// configuration de la persistance
-const persistConfig = {
-  key: "root",
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// Création du store Redux en utilisant configureStore
-const store = configureStore({
-  reducer: persistedReducer, // Utiliser le reducer persistant comme reducer principal
+/**
+ * The store setup with Redux Toolkit
+ * It contains the reducers from the API, authentication slice and user slice
+ */
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+    auth: authReducer,
+    user: userReducer,
+  },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
-
-  devTools: true, // Activr l'extension de devTools
+    getDefaultMiddleware().concat(api.middleware),
 });
-
-// Création du persistor en utilisant persistStore avec le store
-const persistor = persistStore(store);
-
-export { store, persistor }; // Exporter le persistor et le store;
