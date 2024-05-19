cd44e4bdac004cf855893a51c3dba4fd46d53aed
ChabinDev <chabin_972@hotmail.fr>
2024-05-19 22:23:41 +0200

    Mise à jour du Fichier Footer.jsx

--- argentBank/src/components/Footer.jsx
+++ argentBank/src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+/**
+ * Composant Footer
+ * Ce composant affiche le pied de page de l'application.
+ */
 
 const Footer = () => {
+  // Affichage l'année actuelle
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="footer">
-      <p className="footer-text">Copyright 2020 Argent Bank</p>
+      <p className="footer-text">Copyright {currentYear} Argent Bank</p>
     </footer>
   );
 };
