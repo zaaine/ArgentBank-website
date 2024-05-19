1c048af9eb89628d0a334798d083953e72246e7b
ChabinDev <chabin_972@hotmail.fr>
2024-05-19 17:38:12 +0200

    REsctification fichier store.js

--- argentBank/src/app/store.js
+++ argentBank/src/app/store.js
@@ -1,20 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
-// RTK Slices
-import { api } from "./api";
 import authReducer from "./reducers/auth.reducer";
-import userReducer from "./reducers/user.reducer";
 
-/**
- * The store setup with Redux Toolkit
- * It contains the reducers from the API, authentication slice and user slice
- */
 export const store = configureStore({
   reducer: {
-    [api.reducerPath]: api.reducer,
     auth: authReducer,
-    user: userReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
 });
