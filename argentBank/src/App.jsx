2012506a8229c3c2e652c3a3c4079ad8403ad513
ChabinDev <chabin_972@hotmail.fr>
2024-05-19 20:31:59 +0200

    Mise à jour des routes du App.jsx

--- argentBank/src/App.jsx
+++ argentBank/src/App.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 import Logo from "./components/Logo";
 import Footer from "./components/Footer";
-import HomeScreen from "./pages/Home";
-import LoginScreen from "./pages/SignIn";
-import RegisterScreen from "./pages/SignUp";
-import ProfileScreen from "./pages/UserEdit";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import User from "./pages/User";
+import { Navigate, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+Importation du gestionnaire de session
 
 function App() {
   return (
-    <HashRouter>
+    <Router basename={basename}>
       <Routes>
-        <Route path="/home" exact element={<HomeScreen />} />
-        <Route path="/home" exact element={<Logo />} />
-        <Route path="/login" exact element={<LoginScreen />} />
-        <Route path="/register" exact element={<RegisterScreen />} />
-        <Route path="/user-profile" exact element={<ProfileScreen />} />
+        {/* Route normal de l'application */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Logo />} />
+        <Route path="/Sign-in" element={<SignIn />} />
+        <Route path="/Sign-up" element={<SignUp />} />
+        <Route
+          path="/User"
+          element={
+            <PrivateRoute>
+              <User />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/Error404" element={<Error404 />} />
+        {/* Gestion des Routes non reconnus */}
+        <Route path="*" element={<Navigate to="/Error404" replace />} />
       </Routes>
       <Footer />
-    </HashRouter>
+    </Router>
   );
 }
 
