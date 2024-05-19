afe4af221f8d381a24502ed11da58089a3974a98
ChabinDev <chabin_972@hotmail.fr>
2024-05-19 17:15:33 +0200

    Ajout du Router Provider

--- argentBank/src/main.jsx
+++ argentBank/src/main.jsx
@@ -1,28 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "../src/styles/index.css";
 
 // Redux
+import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import rootreducer from "./app/reducers/root.reducer";
 
 // Store
-import process from "process";
-
-const store = configureStore({
-  reducer: rootreducer,
-  devTools: process.env.NODE_ENV !== "production",
-}); // Importer from "./app/store.js";
+import { store } from "./app/store"; // store from "./app/store.js"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <RouterProvider router={App} />
+    </Provider>
   </React.StrictMode>
 );
