79d7e18783ab87f2d8bad02589fd75b99139fc9b
CarmenLoisy <CarmenLoisy@users.noreply.github.com>
2024-05-21 16:37:57 +0200

    mockedTransactions

---
+++ FrontEnd/src/__mocks__/mockedTransactions.js
@@ -0,0 +1,91 @@
+const mockedTransactions = [
+			{
+				transactionId: "134872901",
+				title: "Argent Bank Checking (x8349)",
+				money: "$2,082.79",
+				balanceType: "Available Balance",
+				TransactionDetail: [
+					{
+						date: "2023-05-21T12:00:00Z",
+						description: "Grocery Shopping",
+						transactionType: "Electronic",
+						category: "Food",
+						note: "lorem ipsum"
+					},
+					{
+						date: "2023-06-21T12:00:00Z",
+						description: "Grocery Shopping",
+						transactionType: "Electronic",
+						category: "Food",
+						note: "lorem ipsum"
+					},
+					{
+						date: "2023-07-21T12:00:00Z",
+						description: "Grocery Shopping",
+						transactionType: "Electronic",
+						category: "Food",
+						note: "lorem ipsum"
+					}
+				]
+			},
+			{
+				transactionId: "2473892010",
+				title: "Argent Bank Savings (x6712)",
+				money: "$10,928.42",
+				balanceType: "Available Balance",
+				TransactionDetail: [
+					{
+						date: "2023-05-21T12:00:00Z",
+						description: "Grocery Shopping",
+						transactionType: "Electronic",
+						category: "Food",
+						note: "lorem ipsum"
+					},
+					{
+						date: "2023-06-21T12:00:00Z",
+						description: "Grocery Shopping",
+						transactionType: "Electronic",
+						category: "Food",
+						note: "lorem ipsum"
+					},
+					{
+						date: "2023-07-21T12:00:00Z",
+						description: "Grocery Shopping",
+						transactionType: "Electronic",
+						category: "Food",
+						note: "lorem ipsum"
+					}
+				]
+			},
+			{
+				transactionId: "345879287",
+				title: "Argent Bank Credit Card (x8349)",
+				money: "$184.30",
+				balanceType: "Current Balance",
+				TransactionDetail: [
+					{
+						date: "2023-05-21T12:00:00Z",
+						description: "Grocery Shopping",
+						transactionType: "Electronic",
+						category: "Food",
+						note: "lorem ipsum"
+					},
+					{
+						date: "2023-06-21T12:00:00Z",
+						description: "Grocery Shopping",
+						transactionType: "Electronic",
+						category: "Food",
+						note: "lorem ipsum"
+					},
+					{
+						date: "2023-07-21T12:00:00Z",
+						description: "Grocery Shopping",
+						transactionType: "Electronic",
+						category: "Food",
+						note: "lorem ipsum"
+					}
+				]
+			}
+		]
+  export default mockedTransactions;
+  
