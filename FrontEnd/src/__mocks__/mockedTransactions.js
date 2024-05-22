1f3b96c2aae09d192373c0af30439c1089eb97c1
CarmenLoisy <CarmenLoisy@users.noreply.github.com>
2024-05-22 20:24:24 +0200

    TransactionsMonthResponse

--- FrontEnd/src/__mocks__/mockedTransactions.js
+++ FrontEnd/src/__mocks__/mockedTransactions.js
@@ -1,91 +1,270 @@
 const mockedTransactions = [
-			{
-				transactionId: "134872901",
-				title: "Argent Bank Checking (x8349)",
-				money: "$2,082.79",
-				balanceType: "Available Balance",
-				TransactionDetail: [
-					{
-						date: "2023-05-21T12:00:00Z",
-						description: "Grocery Shopping",
-						transactionType: "Electronic",
-						category: "Food",
-						note: "lorem ipsum"
-					},
-					{
-						date: "2023-06-21T12:00:00Z",
-						description: "Grocery Shopping",
-						transactionType: "Electronic",
-						category: "Food",
-						note: "lorem ipsum"
-					},
-					{
-						date: "2023-07-21T12:00:00Z",
-						description: "Grocery Shopping",
-						transactionType: "Electronic",
-						category: "Food",
-						note: "lorem ipsum"
-					}
-				]
-			},
-			{
-				transactionId: "2473892010",
-				title: "Argent Bank Savings (x6712)",
-				money: "$10,928.42",
-				balanceType: "Available Balance",
-				TransactionDetail: [
-					{
-						date: "2023-05-21T12:00:00Z",
-						description: "Grocery Shopping",
-						transactionType: "Electronic",
-						category: "Food",
-						note: "lorem ipsum"
-					},
-					{
-						date: "2023-06-21T12:00:00Z",
-						description: "Grocery Shopping",
-						transactionType: "Electronic",
-						category: "Food",
-						note: "lorem ipsum"
-					},
-					{
-						date: "2023-07-21T12:00:00Z",
-						description: "Grocery Shopping",
-						transactionType: "Electronic",
-						category: "Food",
-						note: "lorem ipsum"
-					}
-				]
-			},
-			{
-				transactionId: "345879287",
-				title: "Argent Bank Credit Card (x8349)",
-				money: "$184.30",
-				balanceType: "Current Balance",
-				TransactionDetail: [
-					{
-						date: "2023-05-21T12:00:00Z",
-						description: "Grocery Shopping",
-						transactionType: "Electronic",
-						category: "Food",
-						note: "lorem ipsum"
-					},
-					{
-						date: "2023-06-21T12:00:00Z",
-						description: "Grocery Shopping",
-						transactionType: "Electronic",
-						category: "Food",
-						note: "lorem ipsum"
-					},
-					{
-						date: "2023-07-21T12:00:00Z",
-						description: "Grocery Shopping",
-						transactionType: "Electronic",
-						category: "Food",
-						note: "lorem ipsum"
-					}
-				]
-			}
-		]
-  export default mockedTransactions;
-  
+    {
+        accountId: "134872901",
+        title: "Argent Bank Checking (x8349)",
+        money: "$2,082.79",
+        balanceType: "Available Balance",
+        transactions: [
+            {
+                transactionsMonth: "January",
+                transactionDetail: [
+                    { 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    },
+					{ 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    }
+                ]
+            },
+            {
+                transactionsMonth: "February",
+                transactionDetail: [
+                    { 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    },
+					{ 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    }
+                ]
+            },
+            {
+                transactionsMonth: "March",
+                transactionDetail: [
+                    { 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    },
+					{ 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        accountId: "2473892010",
+        title: "Argent Bank Savings (x6712)",
+        money: "$10,928.42",
+        balanceType: "Available Balance",
+        transactions: [
+            {
+                transactionsMonth: "January",
+                transactionDetail: [
+                    { 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    },
+					{ 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    }
+                ]
+            },
+            {
+                transactionsMonth: "February",
+                transactionDetail: [
+                    { 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    },
+					{ 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    }
+                ]
+            },
+            {
+                transactionsMonth: "March",
+                transactionDetail: [
+                    { 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    },
+					{ 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        accountId: "345879287",
+        title: "Argent Bank Credit Card (x8349)",
+        money: "$184.30",
+        balanceType: "Current Balance",
+        transactions: [
+            {
+                transactionsMonth: "January",
+                transactionDetail: [
+                    { 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    },
+					{ 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    }
+                ]
+            },
+            {
+                transactionsMonth: "February",
+                transactionDetail: [
+                    { 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    },
+					{ 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    }
+                ]
+            },
+            {
+                transactionsMonth: "March",
+                transactionDetail: [
+                    { 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    },
+					{ 
+                        date: "2023-05-21T12:00:00Z",
+                        TransactionDescription: "Grocery Shopping",
+                        amount: "50",
+                        balance: "$2187.79",
+                        subProperties: {
+                            transactionType: "Electronic",
+                            category: "Food",
+                            note: "lorem ipsum"
+                        }
+                    }
+                ]
+            },
+        ]
+    }
+];
+export default mockedTransactions;
