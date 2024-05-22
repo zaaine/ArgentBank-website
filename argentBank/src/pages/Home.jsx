534a26e25c9978664938ee1782d52ef68b8e3e05
ChabinDev <142836013+ChabinDev@users.noreply.github.com>
2024-05-22 22:08:08 +0200

    Mise à jour du Fichier Home.jsx

--- argentBank/src/pages/Home.jsx
+++ argentBank/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
 import Features from "../components/Features";
+import PageTitle from '../components/PageTitle'
 
 const Home = () => {
   return (
     <>
+    <PageTitle title="ArgentBank - HomePage" /> {/* Définition du titre de la page */}
       <main>
         <Hero />
         <Features />
