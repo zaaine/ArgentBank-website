30b1b49b19290382751f2790b52ec85acfbfb8c3
ChabinDev <chabin_972@hotmail.fr>
2024-05-23 12:00:28 +0200

    Suppression du fichier configureStore.js

--- argentBank/src/main.jsx
+++ argentBank/src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import { Provider } from "react-redux";
 import "../src/styles/index.css";
@@ -13,9 +12,7 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
