9f0d9fb83b23303ff7dececa7e4d770d070af446
CarmenLoisy <CarmenLoisy@users.noreply.github.com>
2024-05-26 10:04:00 +0200

    Découpage de composants 3 feature homepage

--- FrontEnd/src/components/Feature/Feature.jsx
+++ FrontEnd/src/components/Feature/Feature.jsx
@@ -1,18 +1,49 @@
 import "./feature.scss";
+import iconChat from "../../assets/icon-chat.png";
+import iconMoney from "../../assets/icon-money.png";
+import iconSecurity from "../../assets/icon-security.png";
+import React from 'react';
 import PropTypes from "prop-types";
 
-export default function Feature({ imgSrc, imgAlt, title, description }) {
-	return (
-		<div className="featureItem">
-			<img src={imgSrc} alt={imgAlt} className="featureIcon" />
-			<h3 className="featureItemTitle">{title}</h3>
-			<p>{description}</p>
-		</div>
-	);
-}
-Feature.propTypes = {
-	imgSrc: PropTypes.string.isRequired,
-	imgAlt: PropTypes.string.isRequired,
-	title: PropTypes.string.isRequired,
-	description: PropTypes.string.isRequired,
+export const Feature = ({ title, icon, image, description }) => {
+    return (
+        <div className="featureItem">
+            <h3 className="featureItemTitle">{title}</h3>
+            <img src={image} alt={`${icon} icon`} className="featureIcon" />
+            <p>
+                {description}
+            </p>
+        </div>
+    );
 };
+
+Feature.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+};
+
+// Utilisation des images importées directement
+export const featuresMock = [
+    {
+        title: 'You are our #1 priority',
+        icon: 'chat',
+        description: 'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+        image: iconChat
+    },
+    {
+        title: 'More savings means higher rates',
+        icon: 'money',
+        description: 'The more you save with us, the higher your interest rate will be!',
+        image: iconMoney
+    },
+    {
+        title: 'Security you can trust',
+        icon: 'security',
+        description: 'We use top of the line encryption to make sure your data and money is always safe.',
+        image: iconSecurity
+    }
+];
+
+export default Feature;
--- FrontEnd/src/pages/Home/Home.jsx
+++ FrontEnd/src/pages/Home/Home.jsx
@@ -1,45 +1,33 @@
 import React from "react";
 import "./home.scss";
-import Feature from "../../components/Feature/Feature";
+import Feature, { featuresMock } from "../../components/Feature/Feature";
 import Hero from "../../components/Hero/Hero";
-import iconChat from "../../assets/icon-chat.png";
-import iconMoney from "../../assets/icon-money.png";
-import iconSecurity from "../../assets/icon-security.png";
 
 const Home = () => {
-  return (
-	<main>
-      <div className="hero">
-        <Hero
-          title="Promoted Content"
-          subtitle1="No fees."
-          subtitle2="No minimum deposit."
-          subtitle3="High interest rates."
-          text="Open a savings account with Argent Bank today!"
-        />
-      </div>
-      <section className="features">
-        <h2 className="srOnly">Features</h2>
-        <Feature
-          imgSrc={iconChat}
-          imgAlt="Chat Icon"
-          title="You are our #1 priority"
-          description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
-        />
-        <Feature
-          imgSrc={iconMoney}
-          imgAlt="Money Icon"
-          title="More savings means higher rates"
-          description="The more you save with us, the higher your interest rate will be!"
-        />
-        <Feature
-          imgSrc={iconSecurity}
-          imgAlt="Security Icon"
-          title="Security you can trust"
-          description="We use top of the line encryption to make sure your data and money is always safe."
-        />
-      </section>
-	  </main>
-  );
-}
-export {Home};
+	return (
+		<main>
+			<div className="hero">
+				<Hero
+					title="Promoted Content"
+					subtitle1="No fees."
+					subtitle2="No minimum deposit."
+					subtitle3="High interest rates."
+					text="Open a savings account with Argent Bank today!"
+				/>
+			</div>
+			<section className="features">
+				<h2 className="srOnly">Features</h2>
+				{featuresMock.map((feature) => (
+                    <Feature 
+                        key={feature.title} 
+                        title={feature.title}
+                        icon={feature.icon}
+                        image={feature.image}
+                        description={feature.description}
+                    />
+                ))}
+			</section>
+		</main>
+	);
+};
+export { Home };
