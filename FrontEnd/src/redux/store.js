42abb1f34b80d0472e526f3f69fbf4119760b616
CarmenLoisy <CarmenLoisy@users.noreply.github.com>
2024-05-29 09:29:23 +0200

    Update

--- FrontEnd/src/redux/store.js
+++ FrontEnd/src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { thunk } from 'redux-thunk'; 
 import logger from 'redux-logger';
 import firstNameReducer from './features/firstName';
