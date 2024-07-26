e7338aca37323adda716ed9e0a50a457fc77bdbc
aziz zaaine <zaaine@live.fr>
2024-07-26 14:26:48 +0200

    Mise en place des dépendance et MAJ.

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+const mongoose = require('mongoose');
+const databaseUrl = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/argentBankDB'; // Utiliser l'adresse IPv4 et le port
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(databaseUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true, // Ajouter cette option
+    });
+    console.log('Database successfully connected');
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error(`Database Connectivity Error: ${error}`);
+    throw new Error(error);
   }
-}
+};
